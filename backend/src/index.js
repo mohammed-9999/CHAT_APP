e95f5683b899608e4b6055400892a3ff3e9a5a86
MOHAMMED <arbibm55@gmail.com>
2025-02-22 15:24:58 +0100

    Added message model, controller, and routes for chat functionality

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
 import express from "express"
 import dotenv from "dotenv"
 import authRoutes from"./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
+
 import {connectDB} from "./lib/db.js"
 import cookieParser from "cookie-parser"
 //dotenv.config(); pour acceder a les parametre dans fichier .env
@@ -15,6 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
+app.use("/api/message",messageRoutes);
 
 app.listen(PORT,()=>{
     console.log("server is runing on port :"+ PORT)
